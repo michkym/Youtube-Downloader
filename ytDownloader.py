9df37bccafbfe1968779940d09786428463b9c71
Edward <edwardmukami804@gmail.com>
2022-12-03 10:55:07 +0300

    initial commit, python script only

---
+++ ytDownloader.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+pip install pytube
+
+
+# In[6]:
+
+
+from pytube import YouTube
+from sys import argv
+
+link=input("paste link of youtube video here")
+Storage=input("Paste path to download location")
+yt=YouTube(link)
+
+print("Title: ", yt.title)
+print("Views: ", yt.views)
+
+yd=yt.streams.get_highest_resolution()
+yd.download("D:\Python Src\ytDownloader")
+
+
+# In[ ]:
+
+
+
+
